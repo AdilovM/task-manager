50c49e282aec6a162dd5d2039e05d21d3af06846
adilovm <miras9191@gmail.com>
2023-04-04 12:39:45 -0700

    add task container, btn and btnClick()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+// component declaration
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task-manager';
+  title: string = 'Task Tracker';
 }
